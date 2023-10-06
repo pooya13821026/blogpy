18e56b1957f7efaca6b1ae9411d6e5f6eb1d10b1
pooya gho <poria8210@gmail.com>
2023-10-06 21:39:21 +0330

    create blog and userprofile models and add ckeditor

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='files/user_avatar', null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    img = models.ImageField(upload_to='files/blog_img')
+    content = RichTextField()
+    create_time: models.DateTimeField(editable=False, auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    auther = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
